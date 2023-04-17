ff6fc4fb94709e170a9054a057b5f0ee9db2ef93
Aditya Kendre <kendreaditya@Adityas-MacBook-Pro.local>
2023-04-17 16:38:27 -0400

    multiprocessing image extraction from videos

--- image_process.py
+++ image_process.py
@@ -1,5 +1,8 @@
 import os
 import cv2
+import argparse
+import multiprocessing
+from tqdm import tqdm
 
 def get_frames(name, video_path, output_dir, fps):  # 30 frame, from video to images
     if not os.path.exists(output_dir):
@@ -34,25 +37,36 @@ def resize_images(input_dir, output_dir, width, height):
             cv2.imwrite(resized_img_path, resized_img)
 
 if __name__ == "__main__":
-    import argparse
-    from tqdm import tqdm
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_dir", type=str, default="./data", help="path to dataset directory")
     parser.add_argument("--output_dir", type=str, default="./images", help="path to output directory")
+    parser.add_argument("--width", type=int, default=256, help="output image width")
+    parser.add_argument("--height", type=int, default=256, help="output image height")
+    parser.add_argument("--fps", type=int, default=30, help="desired frame rate")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--cpu", action="store_true", help="use CPU")
+    group.add_argument("--gpu", action="store_true", help="use GPU")
 
     args = parser.parse_args()
+
+    device = "cuda" if args.gpu else "cpu"
 
     names = os.listdir(args.dataset_dir)
     video_form = ['.mp4', '.mov', '.MOV']
 
-    for name in names:
-        dir_path = os.path.join(args.dataset_dir, name)
-        output_images = os.path.join(args.output_dir, name)
-        os.makedirs(dir_path, exist_ok=True)
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        for name in names:
+            dir_path = os.path.join(args.dataset_dir, name)
+            output_images = os.path.join(args.output_dir, name)
+            os.makedirs(output_images, exist_ok=True)
 
-        for file in tqdm(os.listdir(dir_path), desc=f"Processing {name}'s videos"):
-            if file.endswith(tuple(video_form)):
-                video_path = os.path.join(dir_path, file)
-                desired_fps = 30
-                get_frames(name, video_path, output_images, desired_fps)
+            for file in tqdm(os.listdir(dir_path), desc=f"Processing {name}'s videos"):
+                if file.endswith(tuple(video_form)):
+                    video_path = os.path.join(dir_path, file)
+                    pool.apply_async(get_frames, args=(name, video_path, output_images, args.fps))
+
+        pool.close()
+        pool.join()
+
+    resize_images(args.output_dir, args.output_dir, args.width, args.height)
